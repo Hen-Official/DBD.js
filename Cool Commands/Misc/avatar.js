50f8c92b256d920748d7dceee2ddda5f1d57efcd
Hen-Official <76103456+Hen-Official@users.noreply.github.com>
2020-12-16 20:40:59 +0800

    avatar.js
    This is the cool Avatar Command!
    Usage : [prefix]avatar <@mention>

---
+++ Cool Commands/Misc/avatar.js
@@ -0,0 +1,14 @@
+// For Command Handler //
+module.exports = ({
+name: "avatar",
+code: `
+// Basic //
+$image[$authorIcon[$mentioned[1;yes]]
+// Cool Embed Avatar //
+$color[RANDOM]
+$title[Here's Your Avatar That Your Request!]
+$description[$image[$authorIcon[$mentioned[1;yes]]]
+`
+});
+
+// Copyright By Hen //
